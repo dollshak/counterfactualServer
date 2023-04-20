73ef1fce26d091691862762c9ef039f60949fc15
razbam <razbam@post.bgu.ac.il>
2023-04-20 17:57:43 +0300

    implementing handling putput

--- server/serviceLayer/algorithmService.py
+++ server/serviceLayer/algorithmService.py
@@ -21,7 +21,15 @@ class AlgorithmService:
     def run_algorithms(self, algorithms_names, model, arg_list, model_input):
         feature_names, feature_values = InputOutputController().handleInput(model_input)
         # TODO implement here handle output
-        return self.algorithms_controller.run_selected_algorithms(algorithms_names, arg_list, model, feature_values, feature_names)
+        ress = self.algorithms_controller.run_selected_algorithms(algorithms_names, arg_list, model, feature_values, feature_names)
+        output ={}
+        for algo_name ,res in algorithms_names,ress:
+            output[algo_name] = ress[res]
+        input = {}
+        for name,val in zip(feature_names, feature_values):
+            input[name] = val
+        dict = {'input': input,'output':output}
+        return ress
 
     def remove_algorithm(self, name):
         raise Exception("Not implemented.")
